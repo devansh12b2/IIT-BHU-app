744622bb3da53778fe61b34be3df2d9e42e27197
ItsAdityaKSingh <adityakksingh23@gmail.com>
2022-04-15 17:31:59 +0530

    changed options on FAB

--- lib/screens/newScreens/new_home_FAB.dart
+++ lib/screens/newScreens/new_home_FAB.dart
@@ -12,13 +12,13 @@ FabCircularMenu newHomeFAB(BuildContext context, {fabKey}) {
         bottom: screensize.height * 0.025, right: screensize.width * 0.03),
     fabIconBorder: CircleBorder(),
     fabSize: 55.0,
-    fabElevation: 8.0,
+    fabElevation: 10.0,
     animationDuration: Duration(milliseconds: 800),
     ringColor: Color(0xFFb9d8ff),
-    ringDiameter: kIsWeb ? 250.0 : screensize.width * 0.54,
-    ringWidth: kIsWeb ? 50.0 : (screensize.width * 0.8) / 6.6,
+    ringDiameter: kIsWeb ? 250.0 : screensize.width * 0.6,
+    ringWidth: kIsWeb ? 50.0 : (screensize.width * 0.8) / 6,
     fabOpenIcon: Icon(
-      Icons.menu_rounded,
+      Icons.call,
       color: Color(0xFF176ede),
     ),
     fabCloseIcon: Icon(
@@ -28,28 +28,55 @@ FabCircularMenu newHomeFAB(BuildContext context, {fabKey}) {
     fabCloseColor: Color(0xFFb9d8ff),
     fabOpenColor: Color(0xFFb9d8ff),
     children: <Widget>[
-      FloatingItems(image: 'assets/siren.png', number: 'tel:+91 9876543210'),
-      FloatingItems(image: 'assets/police.png', number: 'tel:+91 100'),
-      FloatingItems(image: 'assets/emergency-call.png', number: 'tel:+91 101'),
+      FloatingItems(
+        image: 'assets/police.png',
+        number: 'tel:+91 542236 9134',
+        title: 'Proctor Office',
+      ),
+      FloatingItems(
+        image: 'assets/ambulance.png',
+        number: 'tel:+91 102',
+        title: 'Ambulance',
+      ),
+      FloatingItems(
+        image: 'assets/counselor.png',
+        number: 'tel:+91 75240 53214',
+        title: 'Counselor',
+      ),
     ],
   );
 }
 
 class FloatingItems extends StatelessWidget {
-  const FloatingItems({this.image, this.number});
+  const FloatingItems({this.image, this.number, this.title});
 
   final String image;
   final String number;
+  final String title;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => launch(number),
       child: Container(
-        height: 45.0,
-        decoration: BoxDecoration(
-            color: Color(0xFFd1e6ff),
-            shape: BoxShape.circle,
-            image: DecorationImage(image: AssetImage(image))),
+        height: 55.0,
+        child: Column(
+          children: [
+            Container(
+              height: 35.0,
+              decoration: BoxDecoration(
+                  color: Color(0xFFd1e6ff),
+                  shape: BoxShape.circle,
+                  image: DecorationImage(image: AssetImage(image))),
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                  fontFamily: 'Gilroy',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 11.0),
+            ),
+          ],
+        ),
       ),
     );
   }
